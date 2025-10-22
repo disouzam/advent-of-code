7fc93984c16985adada611e86fa22b7de8200d73
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-10-22 04:11:12 -0300

    Implemented a simple function to read input file and return a list of lines

--- 2018/07/python/main.py
+++ 2018/07/python/main.py
@@ -1,5 +1,12 @@
+def read_input():
+    with open('input.txt', mode='r', encoding='utf-8') as input:
+        lines = input.readlines()
+    return lines
+
 def main():
-    print('It worked')
+    lines = read_input()
+    number_of_lines = len(lines)
+    print(f"Number of lines is {number_of_lines}")
 
 if __name__ == '__main__':
     main()
