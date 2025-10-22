343833f88a2bdea15203148668a9816292e1faa4
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-10-21 23:11:25 -0300

    Simple bash script to create folder for all challenges from the past 10 years and add an empty `.gitignore` in each one

---
+++ create-folder-structure.sh
@@ -0,0 +1,596 @@
+#!/bin/bash
+set -o xtrace
+PS4='${LINENO}: '
+
+mkdir 2015
+cd 2015
+mkdir 01
+echo '' > 01/.gitignore
+mkdir 02
+echo '' > 02/.gitignore
+mkdir 03
+echo '' > 03/.gitignore
+mkdir 04
+echo '' > 04/.gitignore
+mkdir 05
+echo '' > 05/.gitignore
+mkdir 06
+echo '' > 06/.gitignore
+mkdir 07
+echo '' > 07/.gitignore
+mkdir 08
+echo '' > 08/.gitignore
+mkdir 09
+echo '' > 09/.gitignore
+mkdir 10
+echo '' > 10/.gitignore
+mkdir 11
+echo '' > 11/.gitignore
+mkdir 12
+echo '' > 12/.gitignore
+mkdir 13
+echo '' > 13/.gitignore
+mkdir 14
+echo '' > 14/.gitignore
+mkdir 15
+echo '' > 15/.gitignore
+mkdir 16
+echo '' > 16/.gitignore
+mkdir 17
+echo '' > 17/.gitignore
+mkdir 18
+echo '' > 18/.gitignore
+mkdir 19
+echo '' > 19/.gitignore
+mkdir 20
+echo '' > 20/.gitignore
+mkdir 21
+echo '' > 21/.gitignore
+mkdir 22
+echo '' > 22/.gitignore
+mkdir 23
+echo '' > 23/.gitignore
+mkdir 24
+echo '' > 24/.gitignore
+mkdir 25
+echo '' > 25/.gitignore
+
+cd ..
+mkdir 2016
+cd 2016
+mkdir 01
+echo '' > 01/.gitignore
+mkdir 02
+echo '' > 02/.gitignore
+mkdir 03
+echo '' > 03/.gitignore
+mkdir 04
+echo '' > 04/.gitignore
+mkdir 05
+echo '' > 05/.gitignore
+mkdir 06
+echo '' > 06/.gitignore
+mkdir 07
+echo '' > 07/.gitignore
+mkdir 08
+echo '' > 08/.gitignore
+mkdir 09
+echo '' > 09/.gitignore
+mkdir 10
+echo '' > 10/.gitignore
+mkdir 11
+echo '' > 11/.gitignore
+mkdir 12
+echo '' > 12/.gitignore
+mkdir 13
+echo '' > 13/.gitignore
+mkdir 14
+echo '' > 14/.gitignore
+mkdir 15
+echo '' > 15/.gitignore
+mkdir 16
+echo '' > 16/.gitignore
+mkdir 17
+echo '' > 17/.gitignore
+mkdir 18
+echo '' > 18/.gitignore
+mkdir 19
+echo '' > 19/.gitignore
+mkdir 20
+echo '' > 20/.gitignore
+mkdir 21
+echo '' > 21/.gitignore
+mkdir 22
+echo '' > 22/.gitignore
+mkdir 23
+echo '' > 23/.gitignore
+mkdir 24
+echo '' > 24/.gitignore
+mkdir 25
+echo '' > 25/.gitignore
+
+cd ..
+mkdir 2017
+cd 2017
+mkdir 01
+echo '' > 01/.gitignore
+mkdir 02
+echo '' > 02/.gitignore
+mkdir 03
+echo '' > 03/.gitignore
+mkdir 04
+echo '' > 04/.gitignore
+mkdir 05
+echo '' > 05/.gitignore
+mkdir 06
+echo '' > 06/.gitignore
+mkdir 07
+echo '' > 07/.gitignore
+mkdir 08
+echo '' > 08/.gitignore
+mkdir 09
+echo '' > 09/.gitignore
+mkdir 10
+echo '' > 10/.gitignore
+mkdir 11
+echo '' > 11/.gitignore
+mkdir 12
+echo '' > 12/.gitignore
+mkdir 13
+echo '' > 13/.gitignore
+mkdir 14
+echo '' > 14/.gitignore
+mkdir 15
+echo '' > 15/.gitignore
+mkdir 16
+echo '' > 16/.gitignore
+mkdir 17
+echo '' > 17/.gitignore
+mkdir 18
+echo '' > 18/.gitignore
+mkdir 19
+echo '' > 19/.gitignore
+mkdir 20
+echo '' > 20/.gitignore
+mkdir 21
+echo '' > 21/.gitignore
+mkdir 22
+echo '' > 22/.gitignore
+mkdir 23
+echo '' > 23/.gitignore
+mkdir 24
+echo '' > 24/.gitignore
+mkdir 25
+echo '' > 25/.gitignore
+
+cd ..
+mkdir 2018
+cd 2018
+mkdir 01
+echo '' > 01/.gitignore
+mkdir 02
+echo '' > 02/.gitignore
+mkdir 03
+echo '' > 03/.gitignore
+mkdir 04
+echo '' > 04/.gitignore
+mkdir 05
+echo '' > 05/.gitignore
+mkdir 06
+echo '' > 06/.gitignore
+mkdir 07
+echo '' > 07/.gitignore
+mkdir 08
+echo '' > 08/.gitignore
+mkdir 09
+echo '' > 09/.gitignore
+mkdir 10
+echo '' > 10/.gitignore
+mkdir 11
+echo '' > 11/.gitignore
+mkdir 12
+echo '' > 12/.gitignore
+mkdir 13
+echo '' > 13/.gitignore
+mkdir 14
+echo '' > 14/.gitignore
+mkdir 15
+echo '' > 15/.gitignore
+mkdir 16
+echo '' > 16/.gitignore
+mkdir 17
+echo '' > 17/.gitignore
+mkdir 18
+echo '' > 18/.gitignore
+mkdir 19
+echo '' > 19/.gitignore
+mkdir 20
+echo '' > 20/.gitignore
+mkdir 21
+echo '' > 21/.gitignore
+mkdir 22
+echo '' > 22/.gitignore
+mkdir 23
+echo '' > 23/.gitignore
+mkdir 24
+echo '' > 24/.gitignore
+mkdir 25
+echo '' > 25/.gitignore
+
+cd ..
+mkdir 2019
+cd 2019
+mkdir 01
+echo '' > 01/.gitignore
+mkdir 02
+echo '' > 02/.gitignore
+mkdir 03
+echo '' > 03/.gitignore
+mkdir 04
+echo '' > 04/.gitignore
+mkdir 05
+echo '' > 05/.gitignore
+mkdir 06
+echo '' > 06/.gitignore
+mkdir 07
+echo '' > 07/.gitignore
+mkdir 08
+echo '' > 08/.gitignore
+mkdir 09
+echo '' > 09/.gitignore
+mkdir 10
+echo '' > 10/.gitignore
+mkdir 11
+echo '' > 11/.gitignore
+mkdir 12
+echo '' > 12/.gitignore
+mkdir 13
+echo '' > 13/.gitignore
+mkdir 14
+echo '' > 14/.gitignore
+mkdir 15
+echo '' > 15/.gitignore
+mkdir 16
+echo '' > 16/.gitignore
+mkdir 17
+echo '' > 17/.gitignore
+mkdir 18
+echo '' > 18/.gitignore
+mkdir 19
+echo '' > 19/.gitignore
+mkdir 20
+echo '' > 20/.gitignore
+mkdir 21
+echo '' > 21/.gitignore
+mkdir 22
+echo '' > 22/.gitignore
+mkdir 23
+echo '' > 23/.gitignore
+mkdir 24
+echo '' > 24/.gitignore
+mkdir 25
+echo '' > 25/.gitignore
+
+cd ..
+mkdir 2020
+cd 2020
+mkdir 01
+echo '' > 01/.gitignore
+mkdir 02
+echo '' > 02/.gitignore
+mkdir 03
+echo '' > 03/.gitignore
+mkdir 04
+echo '' > 04/.gitignore
+mkdir 05
+echo '' > 05/.gitignore
+mkdir 06
+echo '' > 06/.gitignore
+mkdir 07
+echo '' > 07/.gitignore
+mkdir 08
+echo '' > 08/.gitignore
+mkdir 09
+echo '' > 09/.gitignore
+mkdir 10
+echo '' > 10/.gitignore
+mkdir 11
+echo '' > 11/.gitignore
+mkdir 12
+echo '' > 12/.gitignore
+mkdir 13
+echo '' > 13/.gitignore
+mkdir 14
+echo '' > 14/.gitignore
+mkdir 15
+echo '' > 15/.gitignore
+mkdir 16
+echo '' > 16/.gitignore
+mkdir 17
+echo '' > 17/.gitignore
+mkdir 18
+echo '' > 18/.gitignore
+mkdir 19
+echo '' > 19/.gitignore
+mkdir 20
+echo '' > 20/.gitignore
+mkdir 21
+echo '' > 21/.gitignore
+mkdir 22
+echo '' > 22/.gitignore
+mkdir 23
+echo '' > 23/.gitignore
+mkdir 24
+echo '' > 24/.gitignore
+mkdir 25
+echo '' > 25/.gitignore
+
+cd ..
+mkdir 2021
+cd 2021
+mkdir 01
+echo '' > 01/.gitignore
+mkdir 02
+echo '' > 02/.gitignore
+mkdir 03
+echo '' > 03/.gitignore
+mkdir 04
+echo '' > 04/.gitignore
+mkdir 05
+echo '' > 05/.gitignore
+mkdir 06
+echo '' > 06/.gitignore
+mkdir 07
+echo '' > 07/.gitignore
+mkdir 08
+echo '' > 08/.gitignore
+mkdir 09
+echo '' > 09/.gitignore
+mkdir 10
+echo '' > 10/.gitignore
+mkdir 11
+echo '' > 11/.gitignore
+mkdir 12
+echo '' > 12/.gitignore
+mkdir 13
+echo '' > 13/.gitignore
+mkdir 14
+echo '' > 14/.gitignore
+mkdir 15
+echo '' > 15/.gitignore
+mkdir 16
+echo '' > 16/.gitignore
+mkdir 17
+echo '' > 17/.gitignore
+mkdir 18
+echo '' > 18/.gitignore
+mkdir 19
+echo '' > 19/.gitignore
+mkdir 20
+echo '' > 20/.gitignore
+mkdir 21
+echo '' > 21/.gitignore
+mkdir 22
+echo '' > 22/.gitignore
+mkdir 23
+echo '' > 23/.gitignore
+mkdir 24
+echo '' > 24/.gitignore
+mkdir 25
+echo '' > 25/.gitignore
+
+cd ..
+mkdir 2022
+cd 2022
+mkdir 01
+echo '' > 01/.gitignore
+mkdir 02
+echo '' > 02/.gitignore
+mkdir 03
+echo '' > 03/.gitignore
+mkdir 04
+echo '' > 04/.gitignore
+mkdir 05
+echo '' > 05/.gitignore
+mkdir 06
+echo '' > 06/.gitignore
+mkdir 07
+echo '' > 07/.gitignore
+mkdir 08
+echo '' > 08/.gitignore
+mkdir 09
+echo '' > 09/.gitignore
+mkdir 10
+echo '' > 10/.gitignore
+mkdir 11
+echo '' > 11/.gitignore
+mkdir 12
+echo '' > 12/.gitignore
+mkdir 13
+echo '' > 13/.gitignore
+mkdir 14
+echo '' > 14/.gitignore
+mkdir 15
+echo '' > 15/.gitignore
+mkdir 16
+echo '' > 16/.gitignore
+mkdir 17
+echo '' > 17/.gitignore
+mkdir 18
+echo '' > 18/.gitignore
+mkdir 19
+echo '' > 19/.gitignore
+mkdir 20
+echo '' > 20/.gitignore
+mkdir 21
+echo '' > 21/.gitignore
+mkdir 22
+echo '' > 22/.gitignore
+mkdir 23
+echo '' > 23/.gitignore
+mkdir 24
+echo '' > 24/.gitignore
+mkdir 25
+echo '' > 25/.gitignore
+
+cd ..
+mkdir 2023
+cd 2023
+mkdir 01
+echo '' > 01/.gitignore
+mkdir 02
+echo '' > 02/.gitignore
+mkdir 03
+echo '' > 03/.gitignore
+mkdir 04
+echo '' > 04/.gitignore
+mkdir 05
+echo '' > 05/.gitignore
+mkdir 06
+echo '' > 06/.gitignore
+mkdir 07
+echo '' > 07/.gitignore
+mkdir 08
+echo '' > 08/.gitignore
+mkdir 09
+echo '' > 09/.gitignore
+mkdir 10
+echo '' > 10/.gitignore
+mkdir 11
+echo '' > 11/.gitignore
+mkdir 12
+echo '' > 12/.gitignore
+mkdir 13
+echo '' > 13/.gitignore
+mkdir 14
+echo '' > 14/.gitignore
+mkdir 15
+echo '' > 15/.gitignore
+mkdir 16
+echo '' > 16/.gitignore
+mkdir 17
+echo '' > 17/.gitignore
+mkdir 18
+echo '' > 18/.gitignore
+mkdir 19
+echo '' > 19/.gitignore
+mkdir 20
+echo '' > 20/.gitignore
+mkdir 21
+echo '' > 21/.gitignore
+mkdir 22
+echo '' > 22/.gitignore
+mkdir 23
+echo '' > 23/.gitignore
+mkdir 24
+echo '' > 24/.gitignore
+mkdir 25
+echo '' > 25/.gitignore
+
+cd ..
+mkdir 2024
+cd 2024
+mkdir 01
+echo '' > 01/.gitignore
+mkdir 02
+echo '' > 02/.gitignore
+mkdir 03
+echo '' > 03/.gitignore
+mkdir 04
+echo '' > 04/.gitignore
+mkdir 05
+echo '' > 05/.gitignore
+mkdir 06
+echo '' > 06/.gitignore
+mkdir 07
+echo '' > 07/.gitignore
+mkdir 08
+echo '' > 08/.gitignore
+mkdir 09
+echo '' > 09/.gitignore
+mkdir 10
+echo '' > 10/.gitignore
+mkdir 11
+echo '' > 11/.gitignore
+mkdir 12
+echo '' > 12/.gitignore
+mkdir 13
+echo '' > 13/.gitignore
+mkdir 14
+echo '' > 14/.gitignore
+mkdir 15
+echo '' > 15/.gitignore
+mkdir 16
+echo '' > 16/.gitignore
+mkdir 17
+echo '' > 17/.gitignore
+mkdir 18
+echo '' > 18/.gitignore
+mkdir 19
+echo '' > 19/.gitignore
+mkdir 20
+echo '' > 20/.gitignore
+mkdir 21
+echo '' > 21/.gitignore
+mkdir 22
+echo '' > 22/.gitignore
+mkdir 23
+echo '' > 23/.gitignore
+mkdir 24
+echo '' > 24/.gitignore
+mkdir 25
+echo '' > 25/.gitignore
+
+cd ..
+mkdir 2025
+cd 2025
+mkdir 01
+echo '' > 01/.gitignore
+mkdir 02
+echo '' > 02/.gitignore
+mkdir 03
+echo '' > 03/.gitignore
+mkdir 04
+echo '' > 04/.gitignore
+mkdir 05
+echo '' > 05/.gitignore
+mkdir 06
+echo '' > 06/.gitignore
+mkdir 07
+echo '' > 07/.gitignore
+mkdir 08
+echo '' > 08/.gitignore
+mkdir 09
+echo '' > 09/.gitignore
+mkdir 10
+echo '' > 10/.gitignore
+mkdir 11
+echo '' > 11/.gitignore
+mkdir 12
+echo '' > 12/.gitignore
+mkdir 13
+echo '' > 13/.gitignore
+mkdir 14
+echo '' > 14/.gitignore
+mkdir 15
+echo '' > 15/.gitignore
+mkdir 16
+echo '' > 16/.gitignore
+mkdir 17
+echo '' > 17/.gitignore
+mkdir 18
+echo '' > 18/.gitignore
+mkdir 19
+echo '' > 19/.gitignore
+mkdir 20
+echo '' > 20/.gitignore
+mkdir 21
+echo '' > 21/.gitignore
+mkdir 22
+echo '' > 22/.gitignore
+mkdir 23
+echo '' > 23/.gitignore
+mkdir 24
+echo '' > 24/.gitignore
+mkdir 25
+echo '' > 25/.gitignore
