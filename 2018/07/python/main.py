2c4d699479d0c972360dea6f2b5d8d972f9b2d69
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-10-22 04:37:24 -0300

    Added a function to build a precedence dictionary where the key represents that activity that must be completed before any other activity in the list stored as value

--- 2018/07/python/main.py
+++ 2018/07/python/main.py
@@ -3,10 +3,29 @@ def read_input():
         lines = input.readlines()
     return lines
 
+def build_precedence_dictionary(lines):
+    precedence_dictionary = {}
+    for line in lines:
+        # get the activity
+        activity = line[5]
+        
+        # get the successor
+        successor = line[36]
+        
+        if activity in precedence_dictionary:
+            precedence_dictionary[activity].append(successor)
+        else:
+            precedence_dictionary[activity] = [successor]
+        
+    return precedence_dictionary
+
 def main():
     lines = read_input()
     number_of_lines = len(lines)
     print(f"Number of lines is {number_of_lines}")
+    
+    dict = build_precedence_dictionary(lines)
+    print(dict)
 
 if __name__ == '__main__':
     main()
